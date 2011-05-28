e8c441da0f9dda19499c6368cb18975e4bdf26e3
delphic <delphic.uk@gmail.com>
2011-05-28 11:53:06 +0100

    Added Size Factor (also changed level1 - unimportant)

--- level1.js
+++ level1.js
@@ -1,39 +1,39 @@
 		// Lighting
-		Gremlin.setLightEnvironment(0.4,0.4,0.4,0.4,0.4,0.4,-1.0,0.0,0.0);
+		Gremlin.setLightEnvironment(0.01,0.01,0.01,0.75,0.75,0.6,-1.0,0.0,0.0);
 		Gremlin.setLighting(true);
 		
-		Gremlin.addPointLight(0,0,-6.0,0.0,2.0,0);
-		Gremlin.addPointLight(0,0,-5.5,6.0,2.0,0);
+		//Gremlin.addPointLight(0,0,-6.0,0.0,2.0,0);
+		//Gremlin.addPointLight(0,0,-5.5,6.0,2.0,0);
 
-		Gremlin.addSpotLight(0,0,0,-0.15,0,-1,1,0,1,5,8,2.0);
-		Gremlin.addSpotLight(0,0,-1,0,0,-1,5,1,1,20,45,2.0);
+		//Gremlin.addSpotLight(0,0,0,-0.15,0,-1,1,0,1,5,8,2.0);
+		//Gremlin.addSpotLight(0,0,-1,0,0,-1,5,1,1,20,45,2.0);
 		
 		// Game Objects
-		var cube = new gameObject(-1.5,0,-8.0);
-		var sphere = new gameObject(1.5, 0, -8.0);
-		var teapot = new gameObject(0,0,-5);
+		//var cube = new gameObject(-1.5,0,-8.0);
+		var sphere = new gameObject(0, 0, -8.0);
+		//var teapot = new gameObject(0,0,-5);
 		var skysphere = new gameObject(0,0,0);
 		
-		Gremlin.createPrimitive(cube, "cube", false, true, 1.0, 0, 0);
+		//Gremlin.createPrimitive(cube, "cube", false, true, 1.0, 0, 0);
 		Gremlin.createPrimitive(sphere, "sphere", false, true, 1.5, 30, 30);
-		Gremlin.loadModel(teapot, "Teapot.json", 0.1);
+		//Gremlin.loadModel(teapot, "Teapot.json", 0.1);
 		Gremlin.createPrimitive(skysphere, "sphere", false, true, 1000, 30, 30);
 		
-		gameObjects["cube"] = cube;
+		//gameObjects["cube"] = cube;
 		gameObjects["sphere"] = sphere;
-		gameObjects["teapot"] = teapot;
+		//gameObjects["teapot"] = teapot;
 		gameObjects["skysphere"] = skysphere;
 		
 		// TODO: Move to texture and handle event to Texture List in Engine, store index in gameobject
 		// TODO: Move Texture Filtering to engine globals
-		gameObjects.cube.texture = Gremlin.createTexture("textures/crate.gif");
-		gameObjects.cube.texture.image.onload = function() { Gremlin.handleLoadedTexture(gameObjects.cube.texture, 3); } 
+		//gameObjects.cube.texture = Gremlin.createTexture("textures/crate.gif");
+		//gameObjects.cube.texture.image.onload = function() { Gremlin.handleLoadedTexture(gameObjects.cube.texture, 3); } 
 		
 		gameObjects.sphere.texture = Gremlin.createTexture("textures/earth.jpg");
 		gameObjects.sphere.texture.image.onload = function() { Gremlin.handleLoadedTexture(gameObjects.sphere.texture, 3); }
 		
-		gameObjects.teapot.texture = Gremlin.createTexture("textures/metal.jpg");
-		gameObjects.teapot.texture.image.onload = function() { Gremlin.handleLoadedTexture(gameObjects.teapot.texture, 3); }
+		//gameObjects.teapot.texture = Gremlin.createTexture("textures/metal.jpg");
+		//gameObjects.teapot.texture.image.onload = function() { Gremlin.handleLoadedTexture(gameObjects.teapot.texture, 3); }
 		
 		gameObjects.skysphere.texture = Gremlin.createTexture("textures/huge_stars.png");
 		gameObjects.skysphere.texture.image.onload = function() { Gremlin.handleLoadedTexture(gameObjects.skysphere.texture, 3); }
@@ -41,7 +41,7 @@
 		gameObjects.skysphere.setIsSkyBox(true);
 		
 		//gameObjects.cube.animate = function(elapsed) { this.rotate( ( (75 * elapsed) / 1000.0), 1, 1, 1); };
-		//gameObjects.sphere.animate = function(elapsed) { this.rotate( ( (-50 * elapsed) / 1000.0), 0, 1, 0); };
-		gameObjects.teapot.animate = function(elapsed) { this.rotate( ( (30 * elapsed) / 1000.0), 0,1,0); };
+		gameObjects.sphere.animate = function(elapsed) { this.rotate( ( (5 * elapsed) / 1000.0), 0, 1, 0); };
+		//gameObjects.teapot.animate = function(elapsed) { this.rotate( ( (30 * elapsed) / 1000.0), 0,1,0); };
 		
-		gameObjects.teapot.setShininess(100);
+		//gameObjects.teapot.setShininess(100);
