3a61a70bcb6bbb286f7003e68d7cda10124d4c01
delphic <delphic.uk@gmail.com>
2011-06-26 21:54:14 +0100

    A Couple of Collision Functions Also a first pass at an enemy object, needs re-doing however.

--- Gremlin.js
+++ Gremlin.js
@@ -1741,3 +1741,31 @@ function _GremlinInput() {
 }
 
 var GremlinInput = _GremlinInput();
+
+function _GremlinCollision() {
+	// TODO: Should proabably introduce bounding volumes and save those to objects and do comparisions on those, for now we'll just feed all the data in.
+	function isPointInsideSphere(spherePosition, radius, point) {
+		var difference = [];
+		vec3.subtract(spherePosition, radius, difference);
+		var separation = vec3.length(diffference);
+		if (separation < radius) return true;
+		return false;
+	}
+	function sphereToSphereIntersect(spherePos1, radius1, spherePos2, radius2) {
+		var difference = [];
+		vec3.subtract(spherePos1, spherePos2, difference);
+		var separation = vec3.length(difference);
+		if (separation < (radius1 + radius2)) return true;
+		return false;
+	}
+	// TODO: Bounding Boxes
+	
+	// TODO: High Speed Collisions (check between previous positions and current position)
+	
+	return {
+		isPointInsideSphere: 			isPointInsideSphere,
+		sphereToSphereIntersect:		sphereToSphereIntersect
+	}
+}
+
+var GremlinCollision = _GremlinCollision();
